e310f5fffa896274a3d6b1a15f0ca8a2cb37b44b
Pranjal Sarkari <pranjal.s.sarkari@gmail.com>
2026-03-16 11:33:55 +0530

    resolved

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 const _supabaseURL = "https://wtenziybnqmuevjwoami.supabase.co";
 const _supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind0ZW56aXlibnFtdWV2andvYW1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1MTE2NDQsImV4cCI6MjA4OTA4NzY0NH0.MSaxuJc7LbX0KR-NpD_XLiO5xxl1fPHpPpv9v9MpCr0"; // Your full key
-const _ADMIN_USER = "dm_amroha";
+const _ADMIN_USER = "cms.amr";
 const _ADMIN_PASS = "admin123";
